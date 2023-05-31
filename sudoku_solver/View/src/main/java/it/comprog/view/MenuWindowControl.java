48f450a66f86f5f8f058cea6e56e7917a0e493c1
eryk-poradecki <eryk.poradecki@gmail.com>
2023-05-31 16:12:31 +0200

    feat: added a dropdown and functionalities for language switching

--- sudoku_solver/View/src/main/java/it/comprog/view/MenuWindowControl.java
+++ sudoku_solver/View/src/main/java/it/comprog/view/MenuWindowControl.java
@@ -1,44 +1,40 @@
 package it.comprog.view;
 
+import it.comprog.view.Difficulty.Difficulties;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
-import javafx.scene.Node;
-import javafx.scene.Parent;
-import javafx.scene.Scene;
 import javafx.scene.control.ComboBox;
-import javafx.stage.Stage;
+import javafx.scene.control.Label;
 
-import java.io.IOException;
 import java.net.URL;
-import java.util.Objects;
+import java.util.ListResourceBundle;
+import java.util.Locale;
 import java.util.ResourceBundle;
+
 
 public class MenuWindowControl implements Initializable {
 
     @FXML
-    private ComboBox<Difficulty.Difficulties> difficulty;
+    private ComboBox<String> difficulty;
+    @FXML
+    private ComboBox<String> language;
     private static String chosenDifficulty;
+    private ResourceBundle chosenBundle;
+
+    @FXML
+    private Label author = new Label();
 
     @FXML
     void gameStarted(ActionEvent event) {
-        Difficulty.Difficulties d = difficulty.getSelectionModel().getSelectedItem();
+        String d = difficulty.getSelectionModel().getSelectedItem();
         if (d != null) {
-            chosenDifficulty = d.toString();
+            chosenDifficulty = d;
         } else {
-            chosenDifficulty = "EASY";
+            chosenDifficulty = "Easy";
         }
         System.out.println("Game started! Difficulty: " + chosenDifficulty);
-        try {
-            Parent gamePageParent = FXMLLoader
-                    .load(Objects.requireNonNull(getClass().getResource("GameWindow.fxml")));
-            Scene gamePageScene = new Scene(gamePageParent);
-            Stage gameStage = (Stage) ((Node) event.getSource()).getScene().getWindow();
-            gameStage.setScene(gamePageScene);
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
+        SceneManager.showStage("GameWindow.fxml", chosenBundle);
     }
 
     public static String getChosenDifficulty() {
@@ -47,6 +43,33 @@ public class MenuWindowControl implements Initializable {
 
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
-        difficulty.getItems().setAll(Difficulty.Difficulties.values());
+        chosenBundle = resourceBundle;
+        difficulty.getItems().addAll(chosenBundle.getString(Difficulties.EASY.getLevel()),
+                                     chosenBundle.getString(Difficulties.MEDIUM.getLevel()),
+                                     chosenBundle.getString(Difficulties.HARD.getLevel()));
+
+        language.getItems().addAll(chosenBundle.getString("English"), chosenBundle.getString("Polish"));
+
+        ListResourceBundle authorBundle = (ListResourceBundle) ResourceBundle.getBundle("it.comprog.view.Author");
+        author.setText(authorBundle.toString());
+    }
+
+    @FXML void getChosenLanguage() {
+        Locale locale = new Locale("en");
+        String choice = language.getValue();
+
+        switch (choice) {
+            case "English", "Angielski" -> locale = new Locale("en");
+            case "Polish", "Polski" -> locale = new Locale("pl");
+            default -> { }
+        }
+        Locale.setDefault(locale);
+        chosenBundle = ResourceBundle.getBundle("it.comprog.view.Language");
+
+        ListResourceBundle authorBundle = (ListResourceBundle) ResourceBundle
+                .getBundle("it.comprog.view.Author", locale);
+
+        SceneManager.showStage("MenuWindow.fxml", chosenBundle);
+        author.setText(authorBundle.toString());
     }
 }
