bd4a9cba28bae987cf1168a5a98a062a0deba357
eryk-poradecki <eryk.poradecki@gmail.com>
2023-05-31 16:17:29 +0200

    chore: modified start method to work with SceneManager and i18n

--- sudoku_solver/View/src/main/java/it/comprog/view/Main.java
+++ sudoku_solver/View/src/main/java/it/comprog/view/Main.java
@@ -1,32 +1,29 @@
 package it.comprog.view;
 
 import javafx.application.Application;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Scene;
 import javafx.stage.Stage;
 
-import java.io.IOException;
-import java.net.URL;
+import java.util.Locale;
+import java.util.ResourceBundle;
 
 public class Main extends Application {
 
+    private static ResourceBundle resourceBundle = ResourceBundle.getBundle("it.comprog.view.Language");
+
     @Override
     public void start(Stage stage) {
-        URL fxmlLocation = getClass().getResource("MenuWindow.fxml");
-        FXMLLoader loader = new FXMLLoader(fxmlLocation);
-        try {
-            stage.setScene(new Scene(
-                    loader.load())
-            );
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-        stage.sizeToScene();
-        stage.show();
+        Locale.setDefault(new Locale("en"));
+
+        SceneManager.setStage(stage);
+        SceneManager.showStage("MenuWindow.fxml", resourceBundle);
     }
 
     public static void main(String[] args) {
         launch();
+    }
+
+    public static ResourceBundle getResourceBundle() {
+        return resourceBundle;
     }
 
 }
